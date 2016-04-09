a999146d68d84ae195e6225535b8aef246c20c37
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-09 16:54:23 -0400

    Disable harfbuzz building for freetype

--- build_freetype_osx.sh
+++ build_freetype_osx.sh
@@ -4,7 +4,7 @@ cd freetype_build
 git checkout VER-2-6-3
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D WITH_PNG=OFF -D WITH_ZLIB=ON -D WITH_BZip2=ON ..
+cmake -D CMAKE_BUILD_TYPE=Release -D WITH_PNG=OFF -D WITH_ZLIB=ON -D WITH_HarfBuzz=OFF -D WITH_BZip2=ON ..
 cmake --build .
 cd ../..
 mkdir -p freetype/v2.6.3/x86_64-apple-darwin

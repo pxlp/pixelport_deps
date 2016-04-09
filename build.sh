a74be043045f7914c4f805ef49aa6319d5c8abd2
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-09 14:20:50 -0400

    Start with build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/assimp/assimp.git
+cd assimp
+git checkout v3.2
+cmake -D ASSIMP_BUILD_ASSIMP_TOOLS=OFF -D ASSIMP_BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Release -D "LIBRARY_SUFFIX=" CMakeLists.txt

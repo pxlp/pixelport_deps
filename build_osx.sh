8f5a94017bb4a45051321a7a7e8bee695b8dbb14
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-09 14:41:52 -0400

    Static build of assimp 32 on osx

--- build_osx.sh
+++ build_osx.sh
@@ -1,5 +1,8 @@
-git clone https://github.com/assimp/assimp.git
-cd assimp
+git clone https://github.com/assimp/assimp.git assimp_build
+cd assimp_build
 git checkout v3.2
-cmake -D ASSIMP_BUILD_ASSIMP_TOOLS=OFF -D ASSIMP_BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Release -D "LIBRARY_SUFFIX=" ../CMakeLists.txt
+cmake -D ASSIMP_BUILD_ASSIMP_TOOLS=OFF -D ASSIMP_BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Release -D "LIBRARY_SUFFIX=" CMakeLists.txt
 make
+cd ..
+mkdir -p assimp/v3.2/osx-32
+cp assimp_build/lib/* assimp/v3.2/osx-32/

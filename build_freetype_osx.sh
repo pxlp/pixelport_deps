d6c9fd99bd490b742247e8cdb8dbf2c3bb28a672
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-16 23:06:05 -0400

    Rebuild freetype on osx

---
+++ build_freetype_osx.sh
@@ -0,0 +1,11 @@
+rm -rf freetype_build
+git clone http://git.sv.nongnu.org/r/freetype/freetype2.git freetype_build
+cd freetype_build
+git checkout VER-2-6-3
+mkdir build
+cd build
+cmake -DWITH_PNG=OFF -DWITH_ZLIB=ON -DWITH_HarfBuzz=OFF -DWITH_BZip2=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64  ..
+cmake --build .
+cd ../..
+mkdir -p freetype/v2.6.3/x86_64-apple-darwin
+cp freetype_build/build/libfreetype.a freetype/v2.6.3/x86_64-apple-darwin/libfreetype.a
